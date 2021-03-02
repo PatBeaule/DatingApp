276567f38530c1e2a05d1e5c35d817d7bdc6f06a
Patrice Beaule <patricebeaule@gmail.com>
2021-03-02 16:01:18 -0500

    End of section 16

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,30 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
-import {map} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { User } from './../_models/user';
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if (user){
+        if (user) {
           this.setCurrentUser(user);
         }
       })
-    )
+    );
   }
 
   register(model: any) {
@@ -32,14 +31,15 @@ export class AccountService {
       map((user: User) => {
         if (user) {
           this.setCurrentUser(user);
-          this.currentUserSource.next(user);
         }
-        
       })
-    )
+    );
   }
 
   setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? (user.roles = roles) : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -47,5 +47,9 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
