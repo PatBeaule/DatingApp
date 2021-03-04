edf2ab54c41519857edf95de940cae3d8072377c
Patrice Beaule <patricebeaule@gmail.com>
2021-03-04 09:43:34 -0500

    End of section 17

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
+using API.SignalR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,8 +20,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-
 
 namespace API
 {
@@ -31,9 +30,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
-
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -42,44 +39,31 @@ namespace API
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(_config);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                //app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
-            //THIS IS MY OWN CUSTOM ERROR MIDDLEWARE
             app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            
-            //THIS IS THE ORIGINAL CODE FROM neil, BUT THROWINS CROSS-ORIGIN ERROR
-            // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            //This is the solution by Neil in the Q and A..and it worked
-            app.UseCors(x => x.WithOrigins("https://localhost:4200")
-                .AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private accountService: AccountService) {}
-    ngOnInit() {
-       this.setCurrentUser();
-    }
+  constructor(
+    private accountService: AccountService,
+    private presence: PresenceService
+  ) {}
 
-    setCurrentUser() {
-      const user: User = JSON.parse(localStorage.getItem('user'));
+  ngOnInit() {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
       this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
     }
+  }
 }
